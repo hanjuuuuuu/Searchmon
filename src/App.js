941caded366fdbfd45bf72fc561347181320b01e
hanjuuuuuu <hanjucomss@naver.com>
2022-07-10 00:13:05 +0900

    chart

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import './App.css';
 import { SearchOutlined } from '@ant-design/icons';
 import { Input, Row, Col, Radio, Select, Checkbox, Typography, Button, Form, Modal} from 'antd';
 import 'antd/dist/antd.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { Line } from '@ant-design/plots';
 import axios from 'axios'
+import produce from 'immer';
 
 const { Option } = Select;
 const moment = require('moment')
 
-const plainOptions1 = ['전체', '모바일', 'PC'];
-const plainOptions2 = ['전체', '여성', '남성'];
-const plainOptions3 = ['전체', '~12', '13~18', '19~24', '25~29', '30~34', '35~39', '40~44', '45~49', '50~54', '55~60', '60~'];
+const plainOptions1 = [{label:'전체', value:''}, {label:'모바일', value:'mo'}, {label:'PC', value:'pc'}];
+const plainOptions2 = [{label:'전체', value:''}, {label:'여성', value:'f'}, {label:'남성', value:'m'}];
+const plainOptions3 = [{label: '전체', value: ''}, {label: '~12', value: '1'}, {label: '13~18', value: '2'},{label: '19~24', value: '3'},
+                      {label: '25~29', value: '4'},{label: '30~34', value: '5'},{label: '35~39', value: '6'},{label: '40~44', value: '7'},
+                      {label: '45~49', value: '8'},{label: '50~54', value: '9'},{label: '55~60', value: '10'},{label: '60~', value: '11'}];
+
+const categoryConfig = {data: [
+  {
+  "period": "",
+  "ratio": 0,
+  "category": ""
+  }
+ ],
+ padding: 'auto',
+ xField: 'period',
+ yField: 'ratio',
+ seriesField: 'category',
+ xAxis: {
+   // type: 'timeCat',
+   tickCount: 10,
+ },
+ colorField: 'type', // or seriesField in some cases
+ color: ['#d62728', '#2ca02c', '#efcc2c','#7f4ab9', '#62b9ef'],
+}
 
 let today = new Date();
 let year = String(today.getFullYear()); //년도 
@@ -20,8 +43,9 @@ let day = String(today.getDate()-1) ; //일
 const App = () => {
   const [subnum, setSubnum] = useState(1);
   const [searchnum, setSearchnum] = useState();
+  const [chartInfo, setChartInfo] = useState(categoryConfig);
   const [form] = Form.useForm();
-
+  
   const queryTemplete = {
     startDate: "",
     endDate: "",
@@ -31,10 +55,30 @@ const App = () => {
             groupName: "",
             keywords: []
             
-        }
+        },
+        {
+          groupName: "",
+          keywords: []
+          
+      },
+      {
+        groupName: "",
+        keywords: []
+        
+    },
+    {
+      groupName: "",
+      keywords: []
+      
+  },
+  {
+    groupName: "",
+    keywords: []
+    
+},
     ],
     device: "",
-    ages: [],
+    ages:[],
     gender: ""
  } 
 
@@ -120,7 +164,7 @@ const App = () => {
     }
   };
 
-  const searchcheck = (search) => {    //주제어를 입력하지 않고 검색어부터 입력하려고 할 때의 modal 창
+  const searchcheck = (search) => {    //주제어를 입력하지 않고 검색어부터 입력하려고 할 때의 modal 창 (searchnum 다시 수정하기)
     let getInfo = form.getFieldsValue()
     switch(search){
       case 'subinputdata1':
@@ -158,37 +202,6 @@ const App = () => {
     }
   };
 
-  const onClick = () => { //클릭하면 주제어, 검색어 입력했는지 확인 후에 값 출력
-    let getData = form.getFieldsValue()
-    if (!(getData.subinput1 || getData.subinputdata1 || getData.subinput2 || getData.subinputdata2 || getData.subinput3 || getData.subinputdata3 || getData.subinput4 || getData.subinputdata4)){
-      info()
-    }
-    else
-      //console.log(form.getFieldsValue())
-  
-      queryTemplete.startDate = "2017-01-01"
-      queryTemplete.endDate = "2017-04-30"
-      queryTemplete.timeUnit = getData.timeUnit
-      queryTemplete.keywordGroups[0].groupName = getData.subinput1
-      queryTemplete.keywordGroups[0].keywords.push("한글", "korean")
-     
-      queryTemplete.device = getData.checkrange
-      queryTemplete.ages.push("1","2")
-      queryTemplete.gender = getData.checkgender
-      
-      console.log(queryTemplete)
-
-
-      axios.post("localhost:8080/hanju", 
-        queryTemplete
-      )
-      .then((response) => {console.log(response.data);
-      })
-      .catch((error) => {console.log(error.response)
-      });
-
-  } 
-
   const updatePeriod = (dateFrom) => {  //날짜 문자열 자르기
     let dateTo = moment().subtract(1, 'days').format("YYYYMMDD")
     const currentData = form.getFieldsValue()
@@ -225,15 +238,89 @@ const App = () => {
 
   }
 
-  const updateLayout = () => {    //주간, 월간일때 day 삭제
-    let currentLayout = form.getFieldsValue()
-    currentLayout.dateFromDay.removeIcon()
-    currentLayout.dateToDay.removeIcon()
-    form.setFieldsValue(currentLayout)
+  // const updateLayout = () => {    //주간, 월간일때 day 삭제
+  //   let currentLayout = form.getFieldsValue()
+  //   currentLayout.dateFromDay.removeIcon()
+  //   currentLayout.dateToDay.removeIcon()
+  //   form.setFieldsValue(currentLayout)
+  // }
+
+  // const updateCheck = () => {     //전체를 선택하면 설정안함으로해서 모든 값 주기 
+
+  // }
+
+  const onClick = () => { //클릭하면 주제어, 검색어 입력했는지 확인 후에 값 출력
+    let fieldData = form.getFieldsValue()
+    if (!(fieldData.subinput1 || fieldData.subinputdata1 || fieldData.subinput2 || fieldData.subinputdata2 || fieldData.subinput3 || fieldData.subinputdata3 || fieldData.subinput4 || fieldData.subinputdata4)){
+      info()
+    }
+    else
+      console.log(form.getFieldsValue())
+    
+      const queryData = {...queryTemplete}
+
+  
+      queryData.startDate = fieldData.dateFromYear + "-" + fieldData.dateFromMonth + "-" + fieldData.dateFromDay
+      queryData.endDate = fieldData.dateToYear + "-" + fieldData.dateToMonth + "-" + fieldData.dateToDay
+      queryData.timeUnit = fieldData.timeUnit
+
+      queryData.keywordGroups[0].groupName = fieldData.subinput1
+      queryData.keywordGroups[0].keywords.push(fieldData.subinputdata1)
+      queryData.keywordGroups[1].groupName = fieldData.subinput2
+      queryData.keywordGroups[1].keywords.push(fieldData.subinputdata2)
+      queryData.keywordGroups[2].groupName = fieldData.subinput3
+      queryData.keywordGroups[2].keywords.push(fieldData.subinputdata3)
+      queryData.keywordGroups[3].groupName = fieldData.subinput4
+      queryData.keywordGroups[3].keywords.push(fieldData.subinputdata4)
+      queryData.keywordGroups[4].groupName = fieldData.subinput5
+      queryData.keywordGroups[4].keywords.push(fieldData.subinputdata5)
+     
+      queryData.device = fieldData.device.join()
+      queryData.ages=[]
+      queryData.ages.push(fieldData.ages.join())
+      queryData.gender = fieldData.gender.join()
+
+      console.log(queryData)
+
+
+      axios.post("http://localhost:8080/hanju", 
+        queryData
+      )
+      .then((response) => {
+        console.log(response.data)
+  
+        setChartInfo(produce(categoryConfig, draft => {      //chartInfo의 복사본에 response값 대입
+          draft.data = []
+          // for(var i=0; i<response.data.results[0].data.length; i++) {
+          //   draft.data.push(response.data.results[0].data[i]);
+          // }
+
+          for(var i=0; i<response.data.results.length; i++) {
+            for(var j=0; j<response.data.results[0].data.length; j++) { 
+              const chartInfo = {}
+              chartInfo.period = response.data.results[i].data[j].period
+              chartInfo.ratio = response.data.results[i].data[j].ratio
+              chartInfo.category = response.data.results[i].title  
+              draft.data.push(chartInfo);
+            }   
+          }
+          
+          }))
+      })
+      .catch((error) => {console.log(error)
+      });   
+
   }
 
+  useEffect(() => {
+    console.log("_________________________")
+    console.log(categoryConfig)
+    console.log("_________________________")
+  }, [chartInfo])
+
   return (
-    <Form 
+    <>
+      <Form 
       form={form}
     >
     <div className="site-input-group-wrapper" style={{marginLeft: '30px', marginTop: '50px'}}> 
@@ -350,8 +437,8 @@ const App = () => {
       }}
       >
       <Option value="day">일간</Option>
-      <Option value="week" onClick={updateLayout}>주간</Option>
-      <Option value="month" onClick={updateLayout}>월간</Option>
+      <Option value="week" >주간</Option>
+      <Option value="month" >월간</Option>
       </Select>
       </Form.Item>
       </Row>
@@ -486,17 +573,17 @@ const App = () => {
     <br />
     <br />
     <Typography.Text>  범위  </Typography.Text>
-    <Form.Item name="checkrange"> 
+    <Form.Item name="device"> 
     <Checkbox.Group name = "choice" options={plainOptions1} />
     </Form.Item>
     <br />
     <Typography.Text>  성별  </Typography.Text>
-    <Form.Item name="checkgender"> 
+    <Form.Item name="gender"> 
     <Checkbox.Group name = "choice" options={plainOptions2} />
     </Form.Item>
     <br />
     <Typography.Text>  연령선택  </Typography.Text>
-    <Form.Item name="checkage"> 
+    <Form.Item name="ages"> 
     <Checkbox.Group name = "choice" options={plainOptions3} />
     </Form.Item>
     <br />
@@ -506,8 +593,11 @@ const App = () => {
     </Button>
 
     </div>
-    </Form>
-  );
-    }
+      </Form>
+      <br />
+      <Line {...chartInfo} />
+  </>
+  )
+}
 
 export default App;
